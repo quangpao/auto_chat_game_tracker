31a0f2c45dbed0302b59496fb57664bf5967f20a
quangpao <ddquangbao@gmail.com>
2024-03-31 13:02:31 +0700

    Update config.yml and add default values if it doesn't exist

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const readFile = require("./readFile");
 const YAML = require('yaml');
 const fs = require('fs');
-const file = fs.readFileSync('config.yml', 'utf8');
 
+// Check if config.yml exists, if not, create it and write default values
+if (!fs.existsSync('config.yml')) {
+  const defaultConfig = {
+    INTERVAL: 500
+  };
+  fs.writeFileSync('config.yml', YAML.stringify(defaultConfig));
+  console.log("Config.yml created with default values");
+}
+const file = fs.readFileSync('config.yml', 'utf8');
 
 const config = YAML.parse(file);
 if (!config) {
