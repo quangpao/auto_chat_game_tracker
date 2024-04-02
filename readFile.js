ee0483d53758e33171e49a109eee45993c29e1c5
quangpao <ddquangbao@gmail.com>
2024-04-02 09:47:47 +0700

    Add game types and improve game detection logic

--- readFile.js
+++ readFile.js
@@ -1,66 +1,74 @@
 const fs = require('fs');
 const rl = require('readline');
 const path = require('path');
-let lastDetectedGame = [0, ''];
+const math = require('mathjs');
+const { exec } = require('child_process');
+
+let lastDetectedGame = [0, '', ''];
+let gameTypes = {
+  'First to type the word': 2,
+  'Solve the following': 1
+}
 
 module.exports = function readFile() {
+  try {
+    const appData = process.env.APPDATA;
+    const filePath = path.join(appData, '.minecraft', 'logs', 'latest.log');
+    
+    // Make stream read file line by line
+    const reader = rl.createInterface({
+      input: fs.createReadStream(filePath, { encoding: 'utf8' })
+    });
 
-  const appData = process.env.APPDATA;
-  const filePath = path.join(appData, '.minecraft', 'logs', 'latest.log');
-  // Make stream read file line by line
-  const reader = rl.createInterface({
-    input: fs.createReadStream(filePath, { encoding: 'utf8' })
-  });
-  let lastChatGame = [0, '']; //[timestamp, chatGame]
-  let lastTypeGame = 1; // 1 = guessNumber, 2 = typeString
-  let isGame = false;
-  reader.on('line', (line) => {
-    isGame = false;
-    if (line.includes('First to type the word')) {
-      lastTypeGame = 2;
-      isGame = true;
-    } else if (line.includes('Solve the following')) {
-      lastTypeGame = 1;
-      isGame = true;
-    }
-    if (isGame) {
-      const date = new Date().toISOString().split('T')[0];
-      const timeLine = `${date} ` + line.split(' ')[0].split('[')[1].split(']')[0];
-      const timestamp = new Date(timeLine).getTime() / 1000;
-      if (timestamp > lastChatGame[0]) {
-        let result = '';
-        switch (lastTypeGame) {
-          case 1:
-            result = line.split('Solve the following: ')[1].replace('\r', '');
-            lastChatGame = [timestamp, eval(result)];
-            break;
-          case 2:
-            result = line.split('First to type the word: ')[1].replace('\r', '');
-            lastChatGame = [timestamp, result];
-            break;
+    let lastChatGame = [0, '', '']; //[timestamp, chatGame, time]
+    let lastTypeGame = 1; // 1 = guessNumber, 2 = typeString
+    let isGame = false;
 
+    reader.on('line', (line) => {
+      isGame = false;
+      const gameType = Object.keys(gameTypes).find(type => line.includes(type));
+      if (gameType) {
+        lastTypeGame = gameTypes[gameType];
+        isGame = true;
+      }
+
+      if (isGame) {
+        const date = new Date().toISOString().split('T')[0];
+        const [time] = line.split(' ')[0].match(/\[(.*?)\]/) || [];
+        const timestamp = Date.parse(`${date} ${time}`) / 1000;
+        if (timestamp > lastChatGame[0]) {
+          let result = '';
+          switch (lastTypeGame) {
+            case 1:
+              result = line.split('Solve the following: ')[1].replace('\r', '');
+              lastChatGame = [timestamp, math.evaluate(result), time];
+              break;
+            case 2:
+              result = line.split('First to type the word: ')[1].replace('\r', '');
+              lastChatGame = [timestamp, result, time];
+              break;
+          }
         }
       }
-    }
-  });
+    });
 
-  reader.on('close', () => {
-    const currentTime = new Date().getTime() / 1000;
-    if (currentTime - lastChatGame[0] > 30) {
-      return;
-    }
-    if (lastDetectedGame[1] === lastChatGame[1]) {
-      return;
-    }
-    lastDetectedGame = lastChatGame;
-    console.log("Detected game: ", lastChatGame[1])
-    const { exec } = require('child_process');
-    exec(`echo ${lastChatGame[1]} | clip`, (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
+    reader.on('close', () => {
+      const currentTime = Date.now() / 1000;
+      if (lastDetectedGame[1] === lastChatGame[1]) {
         return;
       }
-    });
-  });
+      lastDetectedGame = lastChatGame;
+      console.log(`[${lastChatGame[2]}] Detected game: ${lastChatGame[1]}`)
+      console.log(`Next game will be at ${new Date((currentTime + 600) * 1000).toISOString().split('T')[1].split('.')[0]}`)
 
+      exec(`echo ${lastChatGame[1]} | clip`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+    });
+  } catch (error) {
+    console.log('Error detected: ', error);
+  }
 }
